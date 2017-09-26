73ba209869ea812a66564c41f0fc0c4d1819d1e1
vol4tim <vol4tim@gmail.com>
2017-09-26 13:49:08 +0400

    upd video

--- src/modules/app/actions.js
+++ src/modules/app/actions.js
@@ -1,6 +1,6 @@
 import Notifications from 'react-notification-system-redux';
 import i18next from 'i18next'
-// import axios from 'axios'
+import axios from 'axios'
 import { Cookies } from 'react-cookie'
 import { SET_LANGUAGE, SET_VIDEO } from './actionTypes'
 
@@ -36,16 +36,16 @@ export function setLanguage(language) {
 
 export function getVideo() {
   return (dispatch) => {
-    dispatch({
-      type: SET_VIDEO,
-      payload: '-lcz521X1PI'
-    })
-    // axios.get('http://aira.life/liveid')
-    //   .then((result) => {
-    //     dispatch({
-    //       type: SET_VIDEO,
-    //       payload: result.data
-    //     })
-    //   })
+    // dispatch({
+    //   type: SET_VIDEO,
+    //   payload: '-lcz521X1PI'
+    // })
+    axios.get('https://static.aira.life/liveid')
+      .then((result) => {
+        dispatch({
+          type: SET_VIDEO,
+          payload: result.data
+        })
+      })
   }
 }
